d0435a96454d9010ecbc5192f14df892935efe94
sergey s <som-ana@yandex.ru>
2023-07-03 01:31:33 +0300

    добавил экземпляр класса DataGenerator

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -10,7 +10,11 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static org.openqa.selenium.Keys.BACK_SPACE;
 
-class DeliveryTest {
+public class DeliveryTest {
+    DataGenerator dataGenerator = new DataGenerator();
+    String date = dataGenerator.generateDate(4);
+    String changeTheDate = dataGenerator.generateDate(10);
+
 
     @BeforeEach
     void setup() {
@@ -22,22 +26,22 @@ class DeliveryTest {
     void otherMeetingDate(){
         $("[data-test-id=city]").$("[type=text]").setValue(DataGenerator.generateCity());
         $("[data-test-id=date]").$("[class=input__control]").doubleClick().sendKeys(BACK_SPACE);
-        $("[data-test-id=date]").$("[class=input__control]").setValue(DataGenerator.generateDate(4));
+        $("[data-test-id=date]").$("[class=input__control]").setValue(date);
         $("[name=name]").setValue(DataGenerator.generateName("ru"));
         $("[name=phone]").setValue(DataGenerator.generatePhone("ru"));
         $(".checkbox__text").click();
         $(".button__text").click();
         $("[data-test-id=success-notification]").shouldBe(visible, Duration.ofSeconds(14));
-        $("[data-test-id=success-notification] [class=notification__content]").shouldHave(exactText("Встреча успешно запланирована на " + DataGenerator.generateDate(4)))
+        $("[data-test-id=success-notification] [class=notification__content]").shouldHave(exactText("Встреча успешно запланирована на " + date))
                 .shouldBe(visible);
         $("[data-test-id=success-notification] [class=icon-button__content]").click();
         $("[data-test-id=date]").$("[class=input__control]").doubleClick().sendKeys(BACK_SPACE);
-        $("[data-test-id=date]").$("[class=input__control]").setValue(DataGenerator.generateDate(10));
+        $("[data-test-id=date]").$("[class=input__control]").setValue(changeTheDate);
         $(".button__text").click();
         $("[data-test-id=replan-notification] [class=notification__content]").shouldHave(exactText("У вас уже запланирована встреча на другую дату. Перепланировать?"))
                 .shouldBe(visible);
         $("[data-test-id=replan-notification] [class=button__content]").click();
-        $("[data-test-id=success-notification] [class=notification__content]").shouldHave(exactText("Встреча успешно запланирована на " + DataGenerator.generateDate(10)))
+        $("[data-test-id=success-notification] [class=notification__content]").shouldHave(exactText("Встреча успешно запланирована на " + changeTheDate))
                 .shouldBe(visible);
     }
     @Test
@@ -45,17 +49,19 @@ class DeliveryTest {
     void currentMeetingDate(){
         $("[data-test-id=city]").$("[type=text]").setValue(DataGenerator.generateCity());
         $("[data-test-id=date]").$("[class=input__control]").doubleClick().sendKeys(BACK_SPACE);
-        $("[data-test-id=date]").$("[class=input__control]").setValue(DataGenerator.generateDate(4));
+        $("[data-test-id=date]").$("[class=input__control]").setValue(date);
         $("[name=name]").setValue(DataGenerator.generateName("ru"));
         $("[name=phone]").setValue(DataGenerator.generatePhone("ru"));
         $(".checkbox__text").click();
         $(".button__text").click();
         $("[data-test-id=success-notification]").shouldBe(visible, Duration.ofSeconds(14));
-        $("[data-test-id=success-notification] [class=notification__content]").shouldHave(exactText("Встреча успешно запланирована на " + DataGenerator.generateDate(4)))
+        $("[data-test-id=success-notification] [class=notification__content]").shouldHave(exactText("Встреча успешно запланирована на " + date))
                 .shouldBe(visible);
-    $("[data-test-id=success-notification] [class=icon-button__content]").click();
+        $("[data-test-id=success-notification] [class=icon-button__content]").click();
+        //следующие 2 шага можно пропустить,
         $("[data-test-id=date]").$("[class=input__control]").doubleClick().sendKeys(BACK_SPACE);
-        $("[data-test-id=date]").$("[class=input__control]").setValue(DataGenerator.generateDate(4));
+        $("[data-test-id=date]").$("[class=input__control]").setValue(date);
+
         $(".button__text").click();
         $("[data-test-id=success-notification] [class=notification__content]").shouldHave(exactText("У вас уже запланирована встреча на эту дату "))
                 .shouldBe(visible);
